a0cd9ef1a923d74af87d10f617a3ebfd3abe7c1c
OllyYa <exrhodo@gmail.com>
2022-11-09 20:13:45 +0300

    Initial commit

---
+++ authapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here. username, firstname, lastname, email
+
+
+class Author(models.Model):
+    username = models.CharField(max_length=64, unique=True)
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    email = models.CharField(
+        max_length=256,
+        unique=True)
