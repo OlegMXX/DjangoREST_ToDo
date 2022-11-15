5c1fecdc01e4e1b55a23817d25e3180894b8ca1d
OllyYa <exrhodo@gmail.com>
2022-11-15 11:55:06 +0300

    + react

---
+++ frontend/src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AuthorList from './components/Author.js'
+import axios from 'axios'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      'authors': []
+    }
+  }
+
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/api/authapp/')
+      .then(response => {
+        const authors = response.data
+          this.setState(
+          {
+           'authors': authors
+          }
+        )
+      }).catch(error => console.log(error))
+  }
+
+
+
+/*
+    const authors = [
+      {
+        'username': 'Hover',
+        'firstname': 'Петр',
+        'lastname': 'Григорьев',
+        'email':'hoverpeter@mail.ru'
+      },
+      {
+        'username': 'Ninja',
+        'firstname': 'Василий',
+        'lastname': 'Иванов',
+        'email':'ninjavano@mail.ru'
+      },
+    ]
+
+
+
+    this.setState(
+      {
+      'authors': authors
+      }
+    )
+  }
+*/
+  
+
+  render () {
+    return (
+      <div>
+        <AuthorList authors={this.state.authors} />
+      </div>
+    )
+  }
+}
+
+export default App;
