c40bb8cd1ff6436c624f99edcc83e5801928c8e5
OllyYa <exrhodo@gmail.com>
2022-11-24 15:50:13 +0300

    l-3

--- todo/urls.py
+++ todo/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from authapp.views import AuthorModelViewSet
+from memoapp.views import ProjectModelViewSet, TodoModelViewSet
 
 router = DefaultRouter()
-router.register('authapp', AuthorModelViewSet)
+router.register('authors', AuthorModelViewSet)
+router.register('project', ProjectModelViewSet)
+router.register('todo', TodoModelViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
