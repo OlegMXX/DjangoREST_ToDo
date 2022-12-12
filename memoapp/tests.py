55c6fd251833125cb0d3a6883bfc09799afbe562
OllyYa <exrhodo@gmail.com>
2022-12-12 16:54:40 +0300

    frontend authentication

--- memoapp/tests.py
+++ memoapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+import requests
+
+response = requests.post('http://127.0.0.1:8000/api-token-auth/', data={'username':'superadmin', 'password': '1'})
+
+print(response.status_code)
+print(response.json()) 
